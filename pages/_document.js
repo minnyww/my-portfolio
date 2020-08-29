a174343904bf65eb5a1346c9d7dcc507228ca39b
minnyww <minrrverse@gmail.com>
2020-08-29 22:41:53 +0700

    [preconnect] img

--- pages/_document.js
+++ pages/_document.js
@@ -27,4 +27,33 @@ export default class MyDocument extends Document {
          sheet.seal();
       }
    }
+
+   render() {
+      return (
+         <Html>
+            <Head>
+               <link
+                  rel='preconnect'
+                  href='https://cdn.dribbble.com/users/931459/screenshots/4696538/18.png'
+               />
+               <link
+                  rel='preconnect'
+                  href='https://cdn.dribbble.com/users/79571/screenshots/4918556/illustrations.png'
+               />{' '}
+               <link
+                  rel='preconnect'
+                  href='https://static.dribbble.com/users/1204750/screenshots/4015227/frame__2_.png'
+               />
+               {/* <link
+                  href='https://fonts.googleapis.com/css2?family=Prompt&display=swap'
+                  rel='stylesheet'
+               /> */}
+            </Head>
+            <body>
+               <Main />
+               <NextScript />
+            </body>
+         </Html>
+      );
+   }
 }
